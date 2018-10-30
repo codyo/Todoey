eae29a0edba275e0ab3b3d7af67a535d6530e92a
Cody Tolmasoff <codytolmasoff@gmail.com>
2018-10-29 18:09:14 -0700

    Initial setup of realm and it's database - pushing a sample set of data

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -19,6 +20,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
 //        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let data = Data()
+        data.name = "Cody"
+        data.age = 45
+        
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(data)
+            }
+        } catch {
+            print("Error initializing new realm, \(error)")
+        }
+        
+        
         
         return true
     }
